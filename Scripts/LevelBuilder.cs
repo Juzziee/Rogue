35846f9b5180d95aba6b52b946a1625cad21aa28
Juzzy <juzzwing@gmail.com>
2018-02-12 20:48:49 +1000

    init commit

---
+++ Scripts/LevelBuilder.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelBuilder : MonoBehaviour {
+
+	static public List<GameObject> roomList;
+
+	public int numRooms;
+	private int roomCount;
+
+	public GameObject cubePrefab;
+
+	// Use this for initialization
+	void Start () {
+		roomList = new List<GameObject> ();
+		Generate ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Generate(){
+		for(roomCount = 0; roomCount <= numRooms; roomCount++){
+			GameObject cube = Instantiate (cubePrefab);
+			roomList.Add (cube);
+			Vector2 spawnPosition = Random.insideUnitCircle * 20;
+			int sizeX = Random.Range (4, 12);
+			int sizeY = Random.Range (4, 8);
+
+			cube.transform.localScale = new Vector3 (sizeX, sizeY, 1);
+			cube.transform.position = spawnPosition;
+
+		}
+
+		Debug.Log (roomList.Count);
+
+	}
+
+	void SeparateRooms(){
+		GameObject roomA;
+		GameObject roomB;
+
+		int dx, dxa, dxb, dy, dya, dyb;
+
+		bool isTouching;
+		/**
+		if(isTouching) {
+			isTouching = false;
+			for(int i = 0; i < roomList.Count; i++) {
+				roomA = roomList[i];
+				for (int j = i + 1; j < roomList.Count; j++) {
+					roomB = roomList [j];
+					Collider2D roomACollider = roomA.GetComponent<Collider2D> ();
+					Collider2D roomBCollider = roomB.GetComponent<Collider2D> ();
+					if (roomACollider.IsTouching (roomBCollider)) {
+						isTouching = true;
+
+						//Debug.Log (dx);
+					}
+				}
+			}
+		}
+		**/
+	}
+}
---
+++ TestDist.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDist : MonoBehaviour {
+
+	public GameObject roomA;
+	public GameObject roomB;
+
+	public Vector2 roomABoundsMin;
+	public Vector2 roomABoundsMax;
+
+	public Vector2 roomBBoundsMin;
+	public Vector2 roomBBoundsMax;
+
+	private Collider2D RoomACol;
+	private Collider2D RoomBCol;
+
+	// Use this for initialization
+	void Start () {
+
+		RoomACol = roomA.GetComponent<Collider2D> ();
+		RoomBCol = roomB.GetComponent<Collider2D> ();
+
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		roomABoundsMin = RoomACol.bounds.min;
+		roomABoundsMax = RoomACol.bounds.max;
+
+		roomBBoundsMin = RoomBCol.bounds.min;
+		roomBBoundsMax = RoomBCol.bounds.max;
+
+		float xdist = roomABoundsMax.x - roomBBoundsMin.x;
+
+		float dx = Mathf.Min (roomABoundsMax.x - roomBBoundsMin.x, roomABoundsMin.x - roomBBoundsMax.x);
+		Debug.Log (dx);
+	}
+}
